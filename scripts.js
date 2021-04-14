9e46ef497750bb1bf22f36a7e08fc9f34d2a0825
Manav Misra <manavm@visionwebsoft.com>
2021-04-14 13:37:25 -0500

    Log input value on submission

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 document.body.appendChild(document.createElement('ul'));
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+  // TODO: Take the text from the input
+  // TODO: Wrap that in an `<li></li>`
+  // TODO: Append that to the `<ul></ul>`
+  console.log(event.target.elements[0].value);
+});
