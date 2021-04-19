fd405c347f135dd5ab787c91fe6319331e2d804b
Manav Misra <manavm@visionwebsoft.com>
2021-04-19 13:42:15 -0500

    Push new item to `todos` and wrap it in an object

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ document.body.appendChild(document.createElement('ul'));
 
 const ul = document.querySelector('ul');
 
+const todos = [];
+
 document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault();
 
@@ -13,6 +15,17 @@ document.querySelector('form').addEventListener('submit', event => {
     <label for="${id}">${event.target.elements[0].value}</label>
     </li>
   `;
+
+  // TODO: Push this new item to `todos` and wrap it in an object.
+  // {task: "My new todo", completed: false, id: }
+
+  todos.push({
+    task: event.target.elements[0].value,
+    completed: false,
+    id: Date.now(),
+  });
+
+  console.log(todos);
 
   // innerHTML - just assign a string that has HTML inside of it
   ul.innerHTML = newToDo;
