88b3b544d4b4b76fed362c81a1aaafbf19ddf293
Manav Misra <manavm@visionwebsoft.com>
2021-04-26 13:38:04 -0500

    Render todos with new tods

--- scripts.js
+++ scripts.js
@@ -1,29 +1,85 @@
-document.body.appendChild(document.createElement('ul'));
+// Variable Declarations
+const ul = document.body.appendChild(document.createElement('ul'));
 
-const ul = document.querySelector('ul');
+const todos = [
+  {
+    userId: 1,
+    id: 1,
+    task: 'delectus aut autem',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 2,
+    task: 'quis ut nam facilis et officia qui',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 3,
+    task: 'fugiat veniam minus',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 4,
+    task: 'et porro tempora',
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 5,
+    task: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 6,
+    task: 'qui ullam ratione quibusdam voluptatem quia omnis',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 7,
+    task: 'illo expedita consequatur quia in',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 8,
+    task: 'quo adipisci enim quam ut ab',
+    completed: true,
+  },
+];
 
-const todos = [];
+// Function Declarations
+function renderTodos(todoItems) {
+  // TODO: Turn a bunch of todos li
+  ul.innerHTML = todoItems
+    .map(
+      todoItem => `
+  <li>
+  <input type="checkbox" id="${todoItem.id}">
+  <label for="${todoItem.id}">${todoItem.task}</label>
+  </li>
+  `,
+    )
+    .join('');
+}
 
+renderTodos(todos);
+
+// Business Logic
 document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault();
 
   const id = Date.now();
 
-  const newToDo = `
-    <li>
-    <input type="checkbox" id="${id}">
-    <label for="${id}">${event.target.elements[0].value}</label>
-    </li>
-  `;
-
   todos.push({
     task: event.target.elements[0].value,
     completed: false,
-    id: Date.now(),
+    id,
   });
 
-  console.log(todos);
-
-  // innerHTML - just assign a string that has HTML inside of it
-  ul.innerHTML = newToDo;
+  renderTodos(todos);
 });
