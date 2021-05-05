6e6235fc7a07c3b1da2f6f29feda81f28038346a
Manav Misra <manavm@visionwebsoft.com>
2021-05-05 13:36:08 -0500

    Move todos to exported data

---
+++ data.js
@@ -0,0 +1,52 @@
+const todos = [
+  {
+    userId: 1,
+    id: 1,
+    task: 'delectus aut autem',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 2,
+    task: 'quis ut nam facilis et officia qui',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 3,
+    task: 'fugiat veniam minus',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 4,
+    task: 'et porro tempora',
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 5,
+    task: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 6,
+    task: 'qui ullam ratione quibusdam voluptatem quia omnis',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 7,
+    task: 'illo expedita consequatur quia in',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 8,
+    task: 'quo adipisci enim quam ut ab',
+    completed: true,
+  },
+];
+
+export default todos;
--- scripts.js
+++ scripts.js
@@ -1,57 +1,8 @@
+import todos from './data.js';
+
 // Variable Declarations
 const ul = document.body.appendChild(document.createElement('ul'));
 const todosCounter = document.querySelector('#todos-counter');
-
-let todos = [
-  {
-    userId: 1,
-    id: 1,
-    task: 'delectus aut autem',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 2,
-    task: 'quis ut nam facilis et officia qui',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 3,
-    task: 'fugiat veniam minus',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 4,
-    task: 'et porro tempora',
-    completed: true,
-  },
-  {
-    userId: 1,
-    id: 5,
-    task: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 6,
-    task: 'qui ullam ratione quibusdam voluptatem quia omnis',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 7,
-    task: 'illo expedita consequatur quia in',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 8,
-    task: 'quo adipisci enim quam ut ab',
-    completed: true,
-  },
-];
 
 // Function Declarations
 function renderTodos(todoItems) {
